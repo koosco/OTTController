76df6b03abfbf9e34f0fefc1c8bd8a06ec078ec9
kth321 <rnxogud136@gmail.com>
2023-11-14 00:23:44 +0900

    동영상 제어 기능 추가 * stop/resume 버튼 설정 필요 * 3.3V로 변경시 videoChangeDiff 수정 필요

--- ottcontroller/ottcontroller.ino
+++ ottcontroller/ottcontroller.ino
@@ -7,11 +7,18 @@ int vol_tmp = 0;
 int volumeChangeDiff = 5;
 int keyboardMute = 9;
 
+// var for video controller
+int videoNum = A3;
+int video_tmp = 0;
+int videoChangeDiff = 20;
+// int keyBoardStopResume = ; // stop/resume key
+
 void setup() {
   Serial.begin(9600);
 
   // keyboard 초기화
   pinMode(keyboardMute, INPUT_PULLUP);
+  pinMode(keyBoardStopResume, INPUT_PULLUP);
 }
 
 void loop() {
@@ -35,5 +42,26 @@ void loop() {
     Keyboard.press('m');
   else
     Keyboard.release('m');
+
+  // Video Control
+  int videoVal = analogRead(videoNum);
+
+  if(videoVal < video_tmp - videoChangeDiff) {
+    // video forward
+    Keyboard.press(KEY_LEFT_ARROW);
+    Keyboard.release(KEY_LEFT_ARROW);
+    video_tmp = videoVal;
+  }else if(videoVal > video_tmp + videoChangeDiff) {
+    // video backward
+    Keyboard.press(KEY_RIGHT_ARROW);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    video_tmp = videoVal;
+  }
+
+  // Video stop/resume button -> This will be integrated to Video Controller
+  if(digitalRead(keyBoardMute) == LOW)
+    Keyboard.press(' ');
+  else
+    Keyboard.release(' ');
 }
 
